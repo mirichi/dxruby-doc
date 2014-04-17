cac7cd7086e6f20e1c86b8c2ebb0128602e8ab44
oedipa-fake <oedipa.fake@outlook.com>
2014-04-17 23:07:58 +0900

    Modify README.md to index.md

--- make-dxruby-doc.rb
+++ make-dxruby-doc.rb
@@ -3,7 +3,7 @@ require './markup'
 
 def make_dxruby_doc(output_dir, template_dir, input_dir)
   m = Markup.new(template_dir)
-  m.publish(output_dir, input_dir, top_link:'README.md', auto_index:true)
+  m.publish(output_dir, input_dir, top_link:'index.md', auto_index:true)
 
   d = Doxme::Doxme.new(Doxme::HTMLRender.new(template_dir))
   d.publish(output_dir, input_dir)
