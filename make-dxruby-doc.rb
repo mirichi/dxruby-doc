43796fadce1a1b194d2cb72f0aaa8d43ceb0b022
oedipa-fake <oedipa.fake@outlook.com>
2014-04-22 23:32:46 +0900

    ルートファイルをindex.htmlに修正

--- make-dxruby-doc.rb
+++ make-dxruby-doc.rb
@@ -19,7 +19,7 @@ end
 
 def make_dxruby_doc(output_dir, template_dir, input_dir)
   m = Markup.new(template_dir)
-  m.publish(output_dir, input_dir, top_link:'README.md', auto_index:true)
+  m.publish(output_dir, input_dir, top_link:'index.md', auto_index:true)
 
   d = Doxme::Doxme.new(Doxme::HTMLRender.new(template_dir))
   d.publish(output_dir, input_dir)
